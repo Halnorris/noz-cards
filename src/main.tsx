5e4ef1f001078381b5373c2daedebab48005dcdd
Halnorris <habnorris@gmail.com>
2025-08-30 21:08:58 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,8 +10,10 @@ import Pending from './pages/Pending'
 import Live from './pages/Live'
 import Auction from './pages/Auction'
 import HowItWorks from '@/pages/HowItWorks'
-import CardPage from '@/pages/CardPage'   // ✅ fixed here
+import CardPage from '@/pages/CardPage'
+import BasketPage from '@/pages/Basket' // ✅ fixed here
 import NotFound from './pages/NotFound'
+import { BasketProvider } from '@/context/basket'
 
 const router = createBrowserRouter([
   {
@@ -25,7 +27,8 @@ const router = createBrowserRouter([
       { path: 'how-it-works', element: <HowItWorks /> }, // ✅ dropped the extra slash
       { path: 'account/live', element: <Live /> },
       { path: 'auctions/:id', element: <Auction /> },
-      { path: 'card/:id', element: <CardPage /> },       // ✅ fixed here
+      { path: 'card/:id', element: <CardPage /> },
+      { path: 'basket', element: <BasketPage /> },// ✅ fixed here
       { path: '*', element: <NotFound /> },
     ],
   },
